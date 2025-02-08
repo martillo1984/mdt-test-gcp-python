1c7b2b08e323527f7f5a731f8044c487f358ce32
Martin <martillo1984@gmail.com>
2025-02-08 14:07:54 -0600

    Init commit 1

---
+++ app.py
@@ -0,0 +1,12 @@
+# [START hello-app]
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def hello():
+  who = request.args.get("who", "World")
+  return f"Hello {who}!\n"
+
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 8080)
+# [END hello-app]
